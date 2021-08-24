5e7445b18fd2bd308354d44c4add7ab649002bb2
mwilson <mjwilsonmd@gmail.com>
2021-08-24 12:15:38 -0400

    hide/reveal

--- projects/memory_puzzle/card.rb
+++ projects/memory_puzzle/card.rb
@@ -1,15 +1,17 @@
 class Card
     attr_reader :face_value, :face_up
-    def initialize(face_value, face_up)
+    def initialize(face_value)
         @face_value = face_value
+        @face_up = @face_value
+    end
+
+    def hide 
         @face_up = false
     end
 
-    def card_value
-        if @face_up
-            @face_value
-        else
-            "_"
-        end
+    def reveal
+        @face_up = true
     end
+    
+
 end
