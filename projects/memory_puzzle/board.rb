dc831dd64fb672865e8f260fcd9da36719c415e9
mwilson <mjwilsonmd@gmail.com>
2021-08-24 14:10:12 -0400

    board

--- projects/memory_puzzle/board.rb
+++ projects/memory_puzzle/board.rb
@@ -1,5 +1,38 @@
 class Board
+    LETTERS = ("A".."Z").to_a
     def initialize(n)
         @grid = Array.new(n) { Array.new(n) }
     end
+
+    def valid?(pos)
+        (0..3).to_a.include?(pos[0]) && (0..3).to_a.include?(pos[1])
+    end
+
+    def [](pos)
+        @grid[pos[0]][pos[1]]
+    end
+
+    def []=(pos, value)
+        @grid[pos[0]][pos[1]] = value
+    end
+
+    def random_letters
+        rand = []
+        8.times do 
+            rand << LETTERS.sample
+        end
+        rand
+    end
+
+    def populate
+        i = 0
+        while i < random_letters.length
+            pos = [] 
+            pos << (0..3).to_a.sample 
+            pos << (0..3).to_a.sample
+            
+        end
+    end
+
+    
 end
